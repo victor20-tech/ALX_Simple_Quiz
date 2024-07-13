f9b1f327ecc6128cfda3c1e335ae09b9e19a83a0
victor-tech20 <victoradomako10@gmail.com>
2024-07-13 14:27:51 +0000

    Add index.html quiz.js styles.css

---
+++ quiz.js
@@ -0,0 +1,31 @@
+//Access Element
+const feedbackElement = document.getElementById("feedback");
+const submitAnswer = document.getElementById("submit-answer");
+
+
+//declaring checkAnswer function
+function checkAnswer() {
+    //declare the corrrect answer variable
+    const correctAnswer = "4";
+    
+    //Access value of the selected answer
+    const selectedRadioButton = document.querySelector('input[name="quiz"]:checked');
+        
+        //Retrieve the user's answer
+        if(selectedRadioButton){
+            let userAnswer = selectedRadioButton.value;
+            //Compare user's answer with the correct answer
+            if(userAnswer === correctAnswer){
+                feedbackElement.textContent = "Correct! Well done.";
+            } else {
+                feedbackElement.textContent = "That's incorrect. Try again!";
+            }
+        } else {
+            //No answer selected
+            feedbackElement.textContent = "Please select an answer.";
+        }
+
+    } 
+
+    //Add Event Listener to the submit button
+    submitAnswer.addEventListener("click", checkAnswer);
